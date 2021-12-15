38fbc7007de887b753a7b62ef17fb5959e36ea3c
YoshikiTakemura <takech.03111@gmail.com>
2021-12-15 11:54:22 +0900

    素数
    10万までの素数を求める。

--- Main.java
+++ Main.java
@@ -1,6 +1,16 @@
 class Main {
-    public static void main(String[] args) {
-      String msg = "Hello World!";
-      System.out.println(msg);
+  public static void main(String[] args) {
+    int i, j;
+
+    System.out.println("Processing...");
+    /* 素数かどうかを判定 */
+    for (i = 2; i <= 100000; ++i) {
+      for (j = 2; j < i; ++j) {
+        if (i % j == 0) {
+          break;
+        }
+      }
     }
+    System.out.println("Processing completed");
   }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Research-for-Java
 For comparative research with other programs.
+### コマンド忘備録
+* ファイルをコンパイルには　`javac （コンパイルしたいファイル名）`
+* ファイルの実行にはコンパイル時に生成された`（コンパイルしたファイル名）.class`ファイルを`java （コンパイルしたファイル名）`で実行する
